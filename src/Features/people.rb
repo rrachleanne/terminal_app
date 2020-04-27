827918161d9665cd743a217d1926f91494765737
Rlee <rachel.williams@leapdev.io>
2020-04-27 17:45:22 +1000

    added test plan and implementation plan pdf

--- src/Features/people.rb
+++ src/Features/people.rb
@@ -42,7 +42,7 @@
     i = 1    
  
     puts "Please enter in #{numPlayers} player names."
-    # while i <= numPlayers do
+    while i <= numPlayers do
       begin
          puts "Player #{i} name?"
          name = gets.chomp
@@ -50,17 +50,15 @@
         
           #raised an exception
        raise 'Player not found' unless players[i-1]!=""
-         print "value: #{players[i-1]}"
           #increment until we reach no. of players
           i += 1
         rescue 
-          puts "Please enter in player"
-          
-          retry
-        
+          puts "Player not found"
+        # retry
+       
       end
 
-    # end
+    end
 
     return players
 
--- src/tests.rb
+++ src/tests.rb
@@ -1,6 +1,6 @@
 
 require "test/unit"
-require_relative "./main.rb"
+# require_relative "./main.rb"
 require_relative "./features/menu.rb"
 require_relative "./features/people.rb"
 require_relative "./features/ball_numbers.rb"
@@ -9,24 +9,15 @@ require_relative "./features/ball_numbers.rb"
 class Game_test < Test::Unit::TestCase
 #must have 'test' at the beginning 
 
-  def test_help
-    
+  def test_newgame
     #first value = expectation | Second value =retun value of that method
     #test input of menu item
     action = kellypool_menu
-    assert_equal "2" , action, "User types 2"
-    assert_equal "2" , action, "User types 2"
+    assert_equal "1" , action, "User types 1 to start game"
+    assert_equal 4 , player_names.length, "Check no. of players entered is four and executes asking for four players"
   end
-  def test_newgame
-          #first value = expectation | Second value =retun value of that method
-          #test input of menu item
-          action = kellypool_menu
-          assert_equal "1" , action, "User types 1 to start game"
-          assert_equal 4 , player_names.length, "Check no. of players entered is four and executes asking for four players"
-          
-  end
-  
-    
+
+
 end
 
 
